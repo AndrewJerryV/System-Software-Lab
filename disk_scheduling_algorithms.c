332bc7c21fa367cc317ba202f3adb85a59b4134e
Andrew Jerry V <142659281+AndrewJerryV@users.noreply.github.com>
2025-08-29 23:34:35 +0530

    Update disk_scheduling_algorithms.c
    Fixed C-SCAN

--- disk_scheduling_algorithms.c
+++ disk_scheduling_algorithms.c
@@ -36,7 +36,6 @@ void scan(int requests[], int n, int head, int disk_size, int direction) {
             break;
         }
     }
-    
     printf("\nSCAN Disk Scheduling Steps:\n");
     if (direction == 1) {
         for (int i = pos; i < n; i++) {
@@ -65,7 +64,7 @@ void scan(int requests[], int n, int head, int disk_size, int direction) {
 }
 
 void c_scan(int requests[], int n, int head, int disk_size) {
-    int total_movement = 0, pos = 0;
+    int total_movement = 0, pos = -1;
     int sorted[n];
     for (int i = 0; i < n; i++) 
         sorted[i] = requests[i];
@@ -92,15 +91,28 @@ void c_scan(int requests[], int n, int head, int disk_size) {
     
     printf("\nC-SCAN Disk Scheduling Steps:\n");
     for (int i = pos; i < n; i++) {
-        printf("Head moves from %d to %d, movement = %d\n", head, sorted[i], abs(sorted[i] - head));
-        total_movement += abs(sorted[i] - head);
-        head = sorted[i];
+        int movement = abs(sorted[i] - head);
+        if (movement > 0) {
+            printf("Head moves from %d to %d, movement = %d\n", head, sorted[i], movement);
+            total_movement += movement;
+            head = sorted[i];
+        }
     }
+
+    int jump_movement = abs(sorted[0] - 0);
+    printf("Head moves from %d to %d (wrap-around), movement = %d\n", head, 0, abs(0-head));
+    total_movement += abs(0-head);
+    head = 0;
+
     for (int i = 0; i < pos; i++) {
-        printf("Head moves from %d to %d, movement = %d\n", head, sorted[i], abs(sorted[i] - head));
-        total_movement += abs(sorted[i] - head);
-        head = sorted[i];
+        int movement = abs(sorted[i] - head);
+        if (movement > 0) { 
+            printf("Head moves from %d to %d, movement = %d\n", head, sorted[i], movement);
+            total_movement += movement;
+            head = sorted[i];
+        }
     }
+
     printf("Total Head Movement (C-SCAN): %d\n", total_movement);
 }
 
@@ -141,11 +153,9 @@ int main() {
     scanf("%d", &disk_size);
     printf("Enter the direction (1 for up, 0 for down): ");
     scanf("%d", &direction);
-    
     fcfs(requests, n, head);
     scan(requests, n, head, disk_size, direction);
     c_scan(requests, n, head, disk_size);
     sstf(requests, n, head);
-    
     return 0;
 }
